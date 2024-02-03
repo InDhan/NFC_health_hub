81be48b407f589006ba01ebe6f33c0ca75a520a1
Murtaza88962 <msogiyawala@gmail.com>
2024-02-03 12:17:17 +0530

    updated pid and vid for scanner

--- app.py
+++ app.py
@@ -14,28 +14,21 @@ app = Flask(__name__)
 scope = ['https://spreadsheets.google.com/feeds',
       'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('keys.json', scope)
-client = gspread.authorize(credentials)
-sheet = client.open('Patient_Dataset').sheet1  # Replace 'Patient_Dataset' with your actual sheet name
+gc = gspread.authorize(credentials)
+sheet = gc.open_by_key('1FCoRib-XsrcSycRvHtEl8xYAV4KsbTXcr5ZkbkuabsY').get_worksheet(0)  # Replace 'Patient_Dataset' with your actual sheet name
 
 # HID Omnikey 5321 V2 device setup
 vendor_id = 0x076b
 product_id = 0x5321
-device = hid.device()
+device = hid.Device(vid=0x076B, pid=0x5320)# add vid and pid for scanner
 device.open(vendor_id, product_id)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def fetch_patient_data(credentials_file, spreadsheet_id, patient_id):
+def fetch_patient_data(patient_id):
     try:
-        # Authenticate with Google Sheets API
-        scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(credentials_file, scope)
-        gc = gspread.authorize(credentials)
-
-        # Open the Google Spreadsheet
-        sheet = gc.open_by_key(spreadsheet_id).get_worksheet(0)
 
         # Find row index of the patient ID
         row_index = None
@@ -100,8 +93,6 @@ def store_data_in_google_sheets(data):
      #sheet.append_row(row)
 
 if __name__ == '__main__':
-    credentials_file = 'keys.json'  
-    spreadsheet_id = '1FCoRib-XsrcSycRvHtEl8xYAV4KsbTXcr5ZkbkuabsY'  
      # Read patient ID from NFC card
     patient_id_bytes = read_nfc_data()
     if patient_id_bytes is None:
@@ -111,7 +102,7 @@ if __name__ == '__main__':
     print("Patient ID from NFC card:", patient_id)
 
     # Fetch patient data using the retrieved patient ID
-    patient_data = fetch_patient_data(credentials_file, spreadsheet_id, patient_id)
+    patient_data = fetch_patient_data(patient_id)
     
     if patient_data:
         print("Patient Data:")
