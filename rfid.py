4f5cc99074e130f96823f31fa2a1ab38ae3f0e15
Murtaza Sogiyawala <msogiyawala@gmail.com>
2024-03-06 20:35:09 +0530

    rfid.py updated

--- rfid.py
+++ rfid.py
@@ -2,10 +2,13 @@ from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
-def read_rfid():
+def read_rfid_id():
     id, text = reader.read()
-    print(f"ID: {id}")
-    print(f"Text: {text}")
+    
+    return id
+
+def read_rfid_string():
+    id, text = reader.read()
     return text
 
 def write_rfid():
@@ -13,3 +16,8 @@ def write_rfid():
     id, text_written = reader.write(text)
     print(f"ID: {id}")
     print(f"Text Written succesfully : {text_written}")
+
+if __name__ == '__main__':
+   rfid_id = read_rfid_id()
+   patient_id = read_rfid_string()
+   print(f'your id is at {rfid_id} and your patient_id is {patient_id}')
